e7e9d6484ad1c0d69e5c17f894424c96d56887d5
Philopatir <mopndq@gmail.com>
2025-03-01 22:31:10 +0200

    4 commit

---
+++ 415.add-strings.py
@@ -0,0 +1,27 @@
+#
+# @lc app=leetcode id=415 lang=python3
+#
+# [415] Add Strings
+#
+
+# @lc code=start
+class Solution:
+  def addStrings(self, num1: str, num2: str) -> str:
+    ans = []
+    carry = 0
+    i = len(num1) - 1
+    j = len(num2) - 1
+
+    while i >= 0 or j >= 0 or carry:
+      if i >= 0:
+        carry += int(num1[i])
+      if j >= 0:
+        carry += int(num2[j])
+      ans.append(str(carry % 10))
+      carry //= 10
+      i -= 1
+      j -= 1
+
+    return ''.join(reversed(ans))
+# @lc code=end
+
