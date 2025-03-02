ec024f845defbaef30a595b1b43035018b523e57
Philopatir <mopndq@gmail.com>
2025-03-02 10:16:56 +0200

    edit

--- 2.add-two-numbers.py
+++ 2.add-two-numbers.py
@@ -10,6 +10,9 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+from typing import Optional
+
+
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         result = ListNode(0)
